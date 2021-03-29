739fd9158c72ababcbdee764591b7b3e8899beb5
anchitctrl <70264806+anchitctrl@users.noreply.github.com>
2021-03-29 12:43:58 +0530

    Create 1D Array

---
+++ Array1D.java
@@ -0,0 +1,29 @@
+package learnJava;
+
+public class ArrayDemo 
+{
+
+	public static void main(String[] args) 
+	{
+//		creating a simple 1D Array
+
+//		int num[]=new int[4];
+//		num[0]=5;
+//		num[1]=15;
+//		num[2]=25;
+//		num[3]=35;
+		
+		//simplest way
+		
+		int num[]= {5,15,25,35};
+		
+		for(int i=0; i<4; i++)
+		{
+			System.out.println(num[i]);
+		}
+		
+		
+		
+	}
+
+}
