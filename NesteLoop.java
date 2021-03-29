b5bca5b52e7070e3d0f0f0a76c68a184989122d9
anchitctrl <70264806+anchitctrl@users.noreply.github.com>
2021-03-29 12:51:51 +0530

    Create NestedLoop.java
    Creating a nested Loop to make a basic Patterns of Star

---
+++ NesteLoop.java
@@ -0,0 +1,21 @@
+package learnJava;
+
+public class nestedLoop {
+	public static void main(String[] args) {
+		
+//	* * * *
+//	* * * *
+//	* * * *
+//	* * * *
+//	Lets try to print this or other patterns
+		
+		
+		for(int j=1; j<=4;j++) {
+			for(int i=1; i<=4;i++) {
+				System.out.print(i+ " " );
+			}
+			System.out.println();
+		}
+	}
+
+}
