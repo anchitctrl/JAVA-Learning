58a6d4182e81c463eafe4e6faea721d1da01f7a3
anchitctrl <70264806+anchitctrl@users.noreply.github.com>
2021-03-29 12:49:07 +0530

    Create If Else Loop

---
+++ IfElse.java
@@ -0,0 +1,26 @@
+package learnJava;
+
+public class ifElseSelection {
+	public static void main(String[] args) {
+		int n=7;
+		
+		
+//		if(false) {
+//			System.out.println("Hello");
+//		}
+//		if(false) {
+//			System.out.println("Bye");
+//		}
+		if(n==0) {
+			System.out.println("Nothing");
+		System.out.println("Byee");
+		}
+		else if (n%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+	}
+
+}
